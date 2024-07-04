b842f50ea5daec15b20a85e29156fea171989a49
iniad <s1f102300379@iniad.org>
2024-07-04 14:02:46 +0900

    Third recovery

--- todo/views.py
+++ todo/views.py
@@ -13,7 +13,7 @@ def index(request):
         task.save()
     tasks = Task.objects.all()
     if request.GET.get('order') == 'due':
-        tasks = Task.objects.order_by('due_at') 
+        tasks = Task.objects.order_by('due_at')
     else:
         tasks = Task.objects.order_by('-posted_at')
     context = {
